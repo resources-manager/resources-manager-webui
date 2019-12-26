8a0e27a287e1dec6d4b8a2f544978e815fb977ff
Gnieark <gnieark@free.fr>
2019-12-26 16:55:43 +0100

    wip

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Entity\User;
@@ -31,4 +36,35 @@ class SecurityController extends AbstractController
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
+
+    /**
+     * @Route("/register", name="register")
+     */
+
+    public function show_register_form()
+    {
+        return $this->render('security/register.html.twig', [
+            'form' => $this->get_registerform()->createView()
+        ]);
+    }
+
+    private function get_registerform()
+    {
+        return $this->createFormBuilder()
+        ->add('email', EmailType::class)
+        ->add('password', PasswordType::class, [])
+        ->add('display_name', TextType::class,[])
+        ->add('save', SubmitType::class)
+        ->getForm();
+
+    }
+
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $form =  $this->get_registerform();
+        $form->handleRequest($request);
+
+
+    }
+
 }
